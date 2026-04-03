53c1a9e6d033700dd8abde568c287e515bfde897
msayyou <84775525+msayyou@users.noreply.github.com>
2026-04-03 15:24:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,6 @@
 # Déploiement : streamlit run app_v2.py
 # Cloud       : push sur GitHub → Streamlit Community Cloud
 # =============================================================================
-import subprocess, sys
-subprocess.run([sys.executable, "-m", "pip", "install",
-  "plotly", "folium", "streamlit-folium",
-  "libpysal", "esda", "mgwr"], check=True)
-
 import streamlit as st
 import numpy as np
 import pandas as pd
